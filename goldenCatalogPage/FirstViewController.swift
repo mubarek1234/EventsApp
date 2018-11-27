08e98549d5649e3d7104a0f2e608d6cb6e38d650
Mubarek Mussa <mubarekmussa@mubareks-mbp>
2018-11-27 01:51:25 -0800

    Initial Commit

---
+++ goldenCatalogPage/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  goldenCatalogPage
+//
+//  Created by Mubarek Mussa on 11/27/18.
+//  Copyright © 2018 eventFind. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ goldenCatalogPage/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  goldenCatalogPage
+//
+//  Created by Mubarek Mussa on 11/27/18.
+//  Copyright © 2018 eventFind. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
